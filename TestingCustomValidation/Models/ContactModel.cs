0f5a757c2106ec5d9727baed23265cbadd103fc9
Gareth Hodgson <garethhodgson123@gmail.com>
2018-05-29 22:38:36 +0900

    Add project files.

---
+++ TestingCustomValidation/Models/ContactModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingCustomValidation.Models
+{
+    public class ContactModel
+    {
+        [CannotBeRedAttribute(ErrorMessage = "Only gmail accounts are allowed!")]
+        public string Message { get; set; }
+    }
+}
